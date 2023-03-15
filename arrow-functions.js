6d5ed8596576a7faf459fa1d9cdbec8aabef2cfa
gallegodev <gallegodev1991@gmail.com>
2023-03-15 21:38:22 +0000

    Just learning

--- arrow-functions.js
+++ arrow-functions.js
@@ -4,11 +4,28 @@
  */
 
 // Vanilla JavaScript Function
-
+function addTwoNumbers(a, b) {
+    return a + b;
+}
+let sum = addTwoNumbers(4, 4);
+console.log(sum);
 // Arrow Function With Parameters
-
+const addTwoNumbers2 = (a, b) => {
+    return a + b;
+}
+let sum2 = addTwoNumbers2(4, 4);
+console.log(sum2);
 // Single Line Arrow Function With Parameters
-
+const addTwoNumbers3 = (a, b) => a + b;
+let sum3 = addTwoNumbers3(4, 4);
+console.log(sum3);
 // Implicit Returns
-
+const saySomething = message => console.log(message);
+saySomething('Hello there!')
 // Returning Multiple Lines
+const returMultipleLines = () => (
+    <p>
+        This is a multiple string
+    </p>
+)
+console.log(returMultipleLines());
